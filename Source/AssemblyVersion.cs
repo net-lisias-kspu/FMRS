5f65a135c3f0dfdb5f391f64d1870f61f29faafe
Jonathan Bayer <linuxgurugamer@gmail.com>
2017-03-10 07:47:54 -0500

    Fixed .version file to use correct record on ksp-avc site

--- Source/AssemblyVersion.cs
+++ Source/AssemblyVersion.cs
@@ -10,9 +10,9 @@
   
  using System.Reflection;
 
- [assembly: AssemblyVersion("1.2.2.0")]
+ [assembly: AssemblyVersion("1.2.2.1")]
   public partial class FMRS_Version_Info
   {
-   public const string beta_version = "0";
-   public const string version_number = "1.2.2.0";
+   public const string beta_version = "1";
+   public const string version_number = "1.2.2.1";
   }
